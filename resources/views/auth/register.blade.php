3944e4c6a3b2d90b313df9f2c91d10cc47aef52b
PradipaJavierFatah <pradipa2210@gmail.com>
2024-11-18 21:59:09 +0700

    add show password in register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -64,8 +64,11 @@
     <link rel="icon" type="image/png" sizes="32x32" href="asset/faviconlogo.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 
 <body class="bg-secondery">
@@ -75,7 +78,7 @@
             <div class="w-full flex justify-center mb-2">
                 <img src="asset/login/Logo.png" alt="" width="200">
             </div>
-    
+
             <div class="flex flex-wrap justify-center">
                 <div class="w-full px-4 lg:w-1/2 xl:w-1/3">
                     <div class="bg-white rounded-xl shadow-lg overflow-hidden mb-6 border-2 border-black">
@@ -87,38 +90,45 @@
                                 <!-- Name -->
                                 <div>
                                     <x-input-label for="name" :value="__('Name')" class="font-Kanit" />
-                                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                                        :value="old('name')" required autofocus autocomplete="name" />
                                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                                 </div>
 
                                 <!-- Email Address -->
-                                <div class="mt-4">
+                                <div class="mt-4 mb-4">
                                     <x-input-label for="email" :value="__('Email')" class="font-Kanit" />
-                                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                                        :value="old('email')" required autocomplete="username" />
                                     <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                 </div>
-
                                 <!-- Password -->
-                                <div class="mt-4">
-                                    <x-input-label for="password" :value="__('Password')" class="font-Kanit" />
-
-                                    <x-text-input id="password" class="block mt-1 w-full"
-                                                    type="password"
-                                                    name="password"
-                                                    required autocomplete="new-password" />
-
-                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                <div class="mb-4 relative">
+                                    <x-input-label for="password" :value="__('Password')"
+                                        class="font-semibold text-gray-700" />
+                                    <div class="relative">
+                                        <x-text-input id="password"
+                                            class="block mt-1 w-full pr-10 border-gray-300 rounded-lg shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                            type="password" name="password" required autocomplete="new-password" />
+                                        <i class="fas fa-eye absolute top-3 right-3 cursor-pointer text-gray-500"
+                                            onclick="togglePassword('password')"></i>
+                                    </div>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-1 text-red-500 text-sm" />
                                 </div>
 
                                 <!-- Confirm Password -->
-                                <div class="mt-4">
-                                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="font-Kanit"/>
-
-                                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                                    type="password"
-                                                    name="password_confirmation" required autocomplete="new-password" />
-
-                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                <div class="mb-6 relative">
+                                    <x-input-label for="password_confirmation" :value="__('Confirm Password')"
+                                        class="font-semibold text-gray-700" />
+                                    <div class="relative">
+                                        <x-text-input id="password_confirmation"
+                                            class="block mt-1 w-full pr-10 border-gray-300 rounded-lg shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-200"
+                                            type="password" name="password_confirmation" required
+                                            autocomplete="new-password" />
+                                        <i class="fas fa-eye absolute top-3 right-3 cursor-pointer text-gray-500"
+                                            onclick="togglePassword('password_confirmation')"></i>
+                                    </div>
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-1 text-red-500 text-sm" />
                                 </div>
 
                                 <div class="flex items-center justify-center mt-4">
@@ -136,11 +146,29 @@
             </div>
         </div>
         <div class="flex justify-center text-black font-medium">
-            <h1>Sudah punya akun? <a href={{ route('login') }}><span class="cursor-pointer underline">Login disini</span></a></h1>
+            <h1>Sudah punya akun? <a href={{ route('login') }}><span class="cursor-pointer underline">Login
+                        disini</span></a></h1>
         </div>
     </section>
-    
+
     {{-- Daftar Akun End --}}
+
+    <script>
+        function togglePassword(id) {
+            const input = document.getElementById(id);
+            const icon = input.nextElementSibling;
+            if (input.type === 'password') {
+                input.type = 'text';
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                input.type = 'password';
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
+        }
+    </script>
+
 </body>
 
 </html>
--- resources/views/home/home-after.blade.php
+++ resources/views/home/home-after.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Testing</title>
+    <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/flowbite@1.5.1/dist/flowbite.min.css" rel="stylesheet">
     @vite('resources/css/app.css')
     <link rel="icon" type="image/png" sizes="32x32" href="asset/faviconlogo.png">
