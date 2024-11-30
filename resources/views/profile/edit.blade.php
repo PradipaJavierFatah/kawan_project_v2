5909891943c35f5c9d336805bb575292a96c7c00
PradipaJavierFatah <pradipa2210@gmail.com>
2024-11-30 23:19:19 +0700

    update profile settings

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -37,6 +37,9 @@
 
     <div class="py-12 bg-gray-100" style="background-image: url('{{ asset('asset/home/bg-1.png') }}'); background-size: cover; background-position: center;">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <h2 class="font-semibold text-2xl text-white leading-tight">
+                {{ __('Settings') }}
+            </h2>
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg bg-opacity-80">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
