164d67bbc3c04a4fab9f6bc9bd77efec6728735c
PradipaJavierFatah <pradipa2210@gmail.com>
2024-12-06 13:19:34 +0700

    style: Add an image to the carousel to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,25 +134,19 @@
                                 <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                                     <!-- Item 1 -->
                                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="{{ asset('asset/home/test1.jpg') }}"
+                                        <img src="{{ asset('asset/home/promo1.png') }}"
                                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                                             alt="...">
                                     </div>
                                     <!-- Item 2 -->
                                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="{{ asset('asset/home/test2.jpg') }}"
+                                        <img src="{{ asset('asset/home/promo2.png') }}"
                                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                                             alt="...">
                                     </div>
                                     <!-- Item 3 -->
                                     <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="{{ asset('asset/home/test3.jpg') }}"
-                                            class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                                            alt="...">
-                                    </div>
-                                    <!-- Item 4 -->
-                                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                        <img src="{{ asset('asset/home/test4.jpeg') }}"
+                                        <img src="{{ asset('asset/home/promo3.png') }}"
                                             class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                                             alt="...">
                                     </div>
@@ -166,10 +160,6 @@
                                         aria-label="Slide 2" data-carousel-slide-to="1"></button>
                                     <button type="button" class="w-3 h-3 rounded-full" aria-current="false"
                                         aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false"
-                                        aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                                    <button type="button" class="w-3 h-3 rounded-full" aria-current="false"
-                                        aria-label="Slide 5" data-carousel-slide-to="4"></button>
                                 </div>
                                 <!-- Slider controls -->
                                 <button type="button"
